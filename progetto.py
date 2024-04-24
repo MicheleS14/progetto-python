af3ce6dcfc0a3a9e1007169cec617968719a6eaf
Vigly1612 <156792365+Vigly1612@users.noreply.github.com>
2024-04-24 12:14:47 +0200

    Update progetto.py
    WEeeeeeeee

--- progetto.py
+++ progetto.py
@@ -1,15 +1,27 @@
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import os
 
+#Costruiamo il percorso del csv da aprire
 current_file = __file__
 current_dir = os.path.dirname(current_file)
+file_dir = os.path.join(current_dir + '\\datadump_s5-000.csv')
 
-file_dir = os.path.join(current_dir + '/datadump_s5-000.csv')
-
-dataset = pd.read_csv(file_dir)
+#Leggiamo il csv e creiamo un dataframe
+dataset = pd.read_csv(file_dir, nrows = 100000)
 df = pd.DataFrame(dataset)
 
-df_Platform = df.groupby(['platform'])
-df_PC = df_Platform[df_Platform['platform'] == 'PC']
+#Creiamo 3 differenti dataframe per ogni modalità di gioco
+df_SecArea = df[df["gamemode"] == "SECURE_AREA"]
+df_Bomb = df[df["gamemode"] == "BOMB"]
+df_Hostage = df[df["gamemode"] == "HOSTAGE"]
+
+#Contiamo quante partite sono state giocate in ogni mappa per ogni modalità
+df_Map_SecArea = df_SecArea.groupby("mapname")["gamemode"].count()
+df_Map_Bomb = df_Bomb.groupby("mapname")["gamemode"].count()
+df_Map_Hostage = df_Hostage.groupby("mapname")["gamemode"].count()
+
+#print(f"\nModalita' Presidio:\n{df_Map_SecArea}\n\nModalita' Bomba:\n{df_Map_Bomb}\n\nModalita' Ostaggio:\n{df_Map_Hostage}")
+
+df_Map_SecArea.plot.bar()
+plt.show()
