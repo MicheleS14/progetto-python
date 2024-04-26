85e368ef73e3eaafcf2fde85bf2a753985bd3810
Vigly1612 <156792365+Vigly1612@users.noreply.github.com>
2024-04-26 17:48:13 +0200

    Update progetto.py

--- progetto.py
+++ progetto.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-import Func
+import r6func
 
 #Costruiamo il percorso del csv da aprire
 current_file = __file__
@@ -10,8 +10,8 @@ current_dir = os.path.dirname(current_file)
 file_dir = os.path.join(current_dir + '\\datadump_s5-000.csv')
 
 #Leggiamo il csv e creiamo un dataframe
-dataset = pd.read_csv(file_dir, nrows = 100000)
-#dataset = pd.read_csv(file_dir)
+#dataset = pd.read_csv(file_dir, nrows = 100000)
+dataset = pd.read_csv(file_dir)
 df = pd.DataFrame(dataset)
 
 #Creiamo 3 differenti dataframe per ogni piattaforma
@@ -42,60 +42,43 @@ df_XBOX_Ostaggio = df_XBOX[df_XBOX["gamemode"] == "HOSTAGE"]
 #Contiamo quante partite sono state giocate in ogni mappa per ogni modalità
 
 #PC
-df_Map_PC_Presidio = Func.played_Match_Maps(df_PC_Presidio)
-df_Map_PC_Bomba = Func.played_Match_Maps(df_PC_Bomba)
-df_Map_PC_Ostaggio = Func.played_Match_Maps(df_PC_Ostaggio)
+df_Map_PC_Presidio = r6func.played_Match_Maps(df_PC_Presidio)
+df_Map_PC_Bomba = r6func.played_Match_Maps(df_PC_Bomba)
+df_Map_PC_Ostaggio = r6func.played_Match_Maps(df_PC_Ostaggio)
 
 #PS4
-df_Map_PS4_Presidio = Func.played_Match_Maps(df_PS4_Presidio)
-df_Map_PS4_Bomba = Func.played_Match_Maps(df_PS4_Bomba)
-df_Map_PS4_Ostaggio = Func.played_Match_Maps(df_PS4_Ostaggio)
+df_Map_PS4_Presidio = r6func.played_Match_Maps(df_PS4_Presidio)
+df_Map_PS4_Bomba = r6func.played_Match_Maps(df_PS4_Bomba)
+df_Map_PS4_Ostaggio = r6func.played_Match_Maps(df_PS4_Ostaggio)
 
 #XBOX
-df_Map_XBOX_Presidio = Func.played_Match_Maps(df_XBOX_Presidio)
-df_Map_XBOX_Bomba = Func.played_Match_Maps(df_XBOX_Bomba)
-df_Map_XBOX_Ostaggio = Func.played_Match_Maps(df_XBOX_Ostaggio)
+df_Map_XBOX_Presidio = r6func.played_Match_Maps(df_XBOX_Presidio)
+df_Map_XBOX_Bomba = r6func.played_Match_Maps(df_XBOX_Bomba)
+df_Map_XBOX_Ostaggio = r6func.played_Match_Maps(df_XBOX_Ostaggio)
 
 #print(f"\nModalita' Presidio:\n{df_Map_SecArea}\n\nModalita' Bomba:\n{df_Map_Bomb}\n\nModalita' Ostaggio:\n{df_Map_Hostage}")
 
-#Grafico partite Presidio
-x = np.arange(16)
-heigth = 0.2
-plt.figure(figsize=(20,20))
-plt.barh(x - heigth, df_Map_PC_Presidio, height=heigth, color="darkblue")
-plt.barh(x, df_Map_PS4_Presidio, height=heigth, color="green")
-plt.barh(x + heigth, df_Map_XBOX_Presidio, height=heigth, color="cyan")
-plt.yticks(x, df_Map_PC_Ostaggio.index)
-plt.title("Numero partite per mappa in ostaggio")
-plt.legend(["PC", "PS4", "XBOX"])
-plt.show()
+"""#Grafico partite Presidio
+Func.show_Map_Played_Chart(df_Map_PC_Presidio, df_Map_PS4_Presidio, df_Map_XBOX_Presidio, "Presidio")
 
 #Grafico partite Bomba
-x = np.arange(16)
-heigth = 0.2
-plt.figure(figsize=(20,20))
-plt.barh(x - heigth, df_Map_PC_Bomba, height=heigth, color="darkblue")
-plt.barh(x, df_Map_PS4_Bomba, height=heigth, color="green")
-plt.barh(x + heigth, df_Map_XBOX_Bomba, height=heigth, color="cyan")
-plt.yticks(x, df_Map_PC_Ostaggio.index)
-plt.title("Numero partite per mappa in Bomba")
-plt.legend(["PC", "PS4", "XBOX"])
-plt.show()
-
+Func.show_Map_Played_Chart(df_Map_PC_Bomba, df_Map_PS4_Bomba, df_Map_XBOX_Bomba, "Bomba")
 
 #Grafico partite Ostaggio
-x = np.arange(16)
-heigth = 0.2
-plt.figure(figsize=(20,20))
-plt.barh(x - heigth, df_Map_PC_Ostaggio, height=heigth, color="darkblue")
-plt.barh(x, df_Map_PS4_Ostaggio, height=heigth, color="green")
-plt.barh(x + heigth, df_Map_XBOX_Ostaggio, height=heigth, color="cyan")
-plt.yticks(x, df_Map_PC_Ostaggio.index)
-plt.title("Numero partite per mappa in ostaggio")
-plt.legend(["PC", "PS4", "XBOX"])
-plt.show()
-
-df_Match = Func.played_Match_Maps(df)
+Func.show_Map_Played_Chart(df_Map_PC_Ostaggio, df_Map_PS4_Ostaggio, df_Map_XBOX_Ostaggio, "Ostaggio")
+"""
+df_Match = r6func.played_Match_Maps(df)
 
 for map in df["mapname"].unique():
-    print(f"\nMappa: {map}\nOperatore + giocato: {Func.operator_Played(df, map).idxmax()}\nOperatore meno giocato: {Func.operator_Played(df, map).idxmin()}")
+    print(f"\nMappa: {map}\nOperatore + giocato: {r6func.operator_Played(df, map).idxmax()}\nOperatore meno giocato: {r6func.operator_Played(df, map).idxmin()}")
+
+
+df_Win_Perc = r6func.win_Rate(df)
+print(df_Win_Perc)
+
+
+media_Round_Partita = df.groupby("matchid")["roundnumber"].max().mean()
+
+df_Pick_Rate = (df.groupby("operator").size() / (df["matchid"].nunique() * media_Round_Partita)) * 100
+
+print(df_Pick_Rate)
