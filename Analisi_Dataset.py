d4a62e29bceaeee2468b13e3e7c951c5d7a137f7
Vigly1612 <156792365+Vigly1612@users.noreply.github.com>
2024-05-09 17:15:45 +0200

    Update Analisi_Dataset.py

--- Analisi_Dataset.py
+++ Analisi_Dataset.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
-import numpy as np
-import scipy
 import r6func
+
 
 #Costruiamo il percorso del csv da aprire
 current_file = __file__
@@ -11,8 +9,8 @@ current_dir = os.path.dirname(current_file)
 file_dir = os.path.join(current_dir + '\\datadump_s5-000.csv')
 
 #Leggiamo il csv e creiamo un dataframe
-dataset = pd.read_csv(file_dir, nrows = 100000)
-#dataset = pd.read_csv(file_dir)
+#dataset = pd.read_csv(file_dir, nrows = 100000)
+dataset = pd.read_csv(file_dir)
 df = pd.DataFrame(dataset)
 
 #Creiamo 3 differenti dataframe per ogni piattaforma
@@ -42,8 +40,9 @@ df_XBOX_Ostaggio = df_XBOX[df_XBOX["gamemode"] == "HOSTAGE"]
 
 #Contiamo quante partite sono state giocate in ogni mappa per ogni modalità
 
-#PC
+#PC (salviamo su excel i dataframe)
 df_Map_PC_Presidio = r6func.played_Match_Maps(df_PC_Presidio)
+
 df_Map_PC_Bomba = r6func.played_Match_Maps(df_PC_Bomba)
 df_Map_PC_Ostaggio = r6func.played_Match_Maps(df_PC_Ostaggio)
 
@@ -57,42 +56,61 @@ df_Map_XBOX_Presidio = r6func.played_Match_Maps(df_XBOX_Presidio)
 df_Map_XBOX_Bomba = r6func.played_Match_Maps(df_XBOX_Bomba)
 df_Map_XBOX_Ostaggio = r6func.played_Match_Maps(df_XBOX_Ostaggio)
 
-#print(f"\nModalita' Presidio:\n{df_Map_SecArea}\n\nModalita' Bomba:\n{df_Map_Bomb}\n\nModalita' Ostaggio:\n{df_Map_Hostage}")
-
-"""#Grafico partite Presidio
-Func.show_Map_Played_Chart(df_Map_PC_Presidio, df_Map_PS4_Presidio, df_Map_XBOX_Presidio, "Presidio")
+#Grafico partite Presidio
+r6func.show_Map_Played_Chart(df_Map_PC_Presidio, df_Map_PS4_Presidio, df_Map_XBOX_Presidio, "Presidio")
 
 #Grafico partite Bomba
-Func.show_Map_Played_Chart(df_Map_PC_Bomba, df_Map_PS4_Bomba, df_Map_XBOX_Bomba, "Bomba")
+r6func.show_Map_Played_Chart(df_Map_PC_Bomba, df_Map_PS4_Bomba, df_Map_XBOX_Bomba, "Bomba")
 
 #Grafico partite Ostaggio
-Func.show_Map_Played_Chart(df_Map_PC_Ostaggio, df_Map_PS4_Ostaggio, df_Map_XBOX_Ostaggio, "Ostaggio")
+r6func.show_Map_Played_Chart(df_Map_PC_Ostaggio, df_Map_PS4_Ostaggio, df_Map_XBOX_Ostaggio, "Ostaggio")
 
 df_Match = r6func.played_Match_Maps(df)
 
-for map in df["mapname"].unique():
-    print(f"\nMappa: {map}\nOperatore + giocato: {r6func.operator_Played(df, map).idxmax()}\nOperatore meno giocato: {r6func.operator_Played(df, map).idxmin()}")
 
+"""for map in df["mapname"].unique():
+    print(f"\nMappa: {map}\nOperatore + giocato: {r6func.operator_Played(df, map).idxmax()}\nOperatore meno giocato: {r6func.operator_Played(df, map).idxmin()}")
+"""
 
 df_Win_Rate = r6func.win_Rate(df)
-print(df_Win_Rate)
+#print(df_Win_Rate)
 
 df_Pick_Rate = r6func.pick_Rate(df)
-print(df_Pick_Rate)
+#print(df_Pick_Rate)
 
 df_Win_And_Pick_Rate = df_Win_Rate.merge(df_Pick_Rate, left_index=True, right_index=True)
 df_Badge = pd.DataFrame({"operator" : ["BOPE-CAPITAO", "BOPE-CAVEIRA", "G.E.O.-JACKAL", "G.E.O.-MIRA", "GIGN-DOC", "GIGN-MONTAGNE", "GIGN-RESERVE", "GIGN-ROOK", "GIGN-TWITCH"," GSG9-BANDIT", "GSG9-BLITZ", "GSG9-IQ", "GSG9-JAGER", "GSG9-RESERVE", "JTF2-BUCK", "JTF2-FROST", "NAVYSEAL-BLACKBEARD", "NAVYSEAL-VALKYRIE", "SAS-MUTE", "SAS-RESERVE", "SAS-SLEDGE", "SAS-SMOKE", "SAS-THATCHER", "SAT-ECHO", "SAT-HIBANA", "SPETSNAZ-FUZE", "SPETSNAZ-GLAZ", "SPETSNAZ-KAPKAN", "SPETSNAZ-RESERVE", "SPETSNAZ-TACHANKA", "SWAT-ASH", "SWAT-CASTLE", "SWAT-PULSE", "SWAT-RESERVE", "SWAT-THERMITE"],
          "badge" : [os.path.join(current_dir + "\\Badge\\BADGE_Capitao.png"), os.path.join(current_dir + "\\Badge\\BADGE_Caveira.png"), os.path.join(current_dir + "\\Badge\\BADGE_Jackal.png"), os.path.join(current_dir + "\\Badge\\BADGE_Mira.png"), os.path.join(current_dir + "\\Badge\\BADGE_Doc.png"), os.path.join(current_dir + "\\Badge\\BADGE_Montagne.png"), os.path.join(current_dir + "\\Badge\\BADGE_Reserve.png"), os.path.join(current_dir + "\\Badge\\BADGE_Rook.png"), os.path.join(current_dir + "\\Badge\\BADGE_Twitch.png"), os.path.join(current_dir + "\\Badge\\BADGE_Bandit.png"), os.path.join(current_dir + "\\Badge\\BADGE_Blitz.png"), os.path.join(current_dir + "\\Badge\\BADGE_IQ.png"), os.path.join(current_dir + "\\Badge\\BADGE_Jager.png"), os.path.join(current_dir + "\\Badge\\BADGE_Reserve.png"), os.path.join(current_dir + "\\Badge\\BADGE_Buck.png"), os.path.join(current_dir + "\\Badge\\BADGE_Frost.png"), os.path.join(current_dir + "\\Badge\\BADGE_Blackbeard.png"), os.path.join(current_dir + "\\Badge\\BADGE_Valkyrie.png"), os.path.join(current_dir + "\\Badge\\BADGE_Mute.png"), os.path.join(current_dir + "\\Badge\\BADGE_Reserve.png"), os.path.join(current_dir + "\\Badge\\BADGE_Sledge.png"), os.path.join(current_dir + "\\Badge\\BADGE_Smoke.png"), os.path.join(current_dir + "\\Badge\\BADGE_Thatcher.png"), os.path.join(current_dir + "\\Badge\\BADGE_Echo.png"), os.path.join(current_dir + "\\Badge\\BADGE_Hibana.png"), os.path.join(current_dir + "\\Badge\\BADGE_Fuze.png"), os.path.join(current_dir + "\\Badge\\BADGE_Glaz.png"), os.path.join(current_dir + "\\Badge\\BADGE_Kapkan.png"), os.path.join(current_dir + "\\Badge\\BADGE_Reserve.png"), os.path.join(current_dir + "\\Badge\\BADGE_Tachanka.png"), os.path.join(current_dir + "\\Badge\\BADGE_Ash.png"), os.path.join(current_dir + "\\Badge\\BADGE_Castle.png"), os.path.join(current_dir + "\\Badge\\BADGE_Pulse.png"), os.path.join(current_dir + "\\Badge\\BADGE_Reserve.png"), os.path.join(current_dir + "\\Badge\\BADGE_Thermite.png")
 ]}).set_index("operator")
 
+
 df_Win_And_Pick_Rate_Badge = df_Win_And_Pick_Rate.merge(df_Badge, left_index=True, right_index=True)
 
-print(df_Win_And_Pick_Rate_Badge)
+#print(df_Win_And_Pick_Rate_Badge)
 
-r6func.show_Operator_Pick_Win_Rate(df_Win_And_Pick_Rate_Badge)
+print(r6func.show_Operator_Pick_Win_Rate(df_Win_And_Pick_Rate_Badge))
 
-####.to_excel(current_dir + "\\File Risultato.xlsx")
-"""
 
-print(r6func.death_Probability(df, 5, 3))
-#print(df["matchid"].nunique())
+
+print(df["matchid"].nunique())
+
+
+# create a excel writer object
+with pd.ExcelWriter(current_dir + "\\File Risultato.xlsx") as writer:
+
+    #numero partite per mappa 
+    df_Match.to_excel(writer, sheet_name="Numero partite per ogni mappa", startrow=2, startcol=1)
+
+    #partite su pc per mappa
+    df_Map_PC_Presidio.to_excel(writer, sheet_name="Dataframe PC", startrow=2, startcol=1)
+    df_Map_PC_Bomba.to_excel(writer, sheet_name="Dataframe PC", startrow=2, startcol=4)
+    df_Map_PC_Ostaggio.to_excel(writer, sheet_name="Dataframe PC", startrow=2, startcol=7)
+
+    #win & pickrate
+    df_Win_And_Pick_Rate.to_excel(writer, sheet_name="Win & Pick Rate", startrow=2, startcol=1)
+
+    #media kill per round
+    r6func.kills_Mean_For_Round(df, significance_Level=0.05).to_excel(writer, sheet_name="Media di kill per round", startrow=2, startcol=1)
+
+    #Probabilità di morire
+    pd.DataFrame(r6func.death_Probability(df, 20, 12)).to_excel(writer, sheet_name="Probabilità di morire 12 o meno volte in 20 round", startrow=2, startcol=1)
